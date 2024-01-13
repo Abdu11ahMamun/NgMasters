caf087770f0125fbe2cb7b33ffe0e9781f019b19
abdu11ah-mamun <cs.abdullah.mamun@gmail.com>
2024-01-14 02:01:22 +0600

    Data Binding Examples

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,8 +8,25 @@ import { Component } from '@angular/core';
 export class ServersComponent {
   name = 'Abdullah';
   id = 104;
+  allowNewServers = false;
+  OnActive = 'Ready to go';
+  serverName = '';
+  constructor (){
+    setTimeout(()=>{
+      this.allowNewServers = true;
+    },2000)
+  }
 
   methodTesting(){
     return 'Successful Method Testing!';
+  }
+  OnActivateServer(){
+    this.OnActive= 'Busy!!';
+  }
+
+  // "$even" is something that gives us access to date 
+  OnEventBinding(event:Event){
+    this.serverName = (<HTMLInputElement>event.target).value;
+
   }
 }
